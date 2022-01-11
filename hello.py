c70c166506aab5876c67e2500f85116be7375105
Nick <tanlingyu2014@gmail.com>
2022-01-11 17:19:22 +0000

    Update

---
+++ hello.py
@@ -0,0 +1,12 @@
+print('Hello World!')
+
+n = 5
+
+while n >= 0:
+    print(n)
+    n = n - 1
+print('Blastoff!')
+
+# nam = input('Who are you?\n')
+# print('Welcome', nam, '!')
+# print(f"Welcome, {nam}!")
